efef95f3496d00dea9ae0e1947c28f405f9ae960
Emre Türe <emre_ture17@hotmail.com>
2022-04-17 01:05:05 +0300

    Add project files.

---
+++ SubtractionWithoutUsingTheMinusOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SubtractionWithoutUsingTheMinusOperator
+{
+    internal class Program
+    {
+        private static int Subtract(int x, int y)
+        {
+            if (y == 0)
+                return x;
+
+            return Subtract(x ^ y, (~x & y) << 1);
+        }
+        static void Main(string[] args)
+        {
+            int x = 0, y = 0;
+            Console.Write("1.Number =>");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2.Number =>");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"{x} - {y} = {Subtract(x, y)}");
+            Console.ReadLine();
+        }
+    }
+}
